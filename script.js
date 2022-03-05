14904dfd27821b1f8086d10deb89edeacfe65e57
m-dorsey <dorseymakenzie@gmail.com>
2022-03-05 13:05:08 -0500

    initialized and styled game board

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ let currentGuess = [];
 let nextLetter = 0;
 let rightGuessStr = WORDS[ Math.floor( Math.random() * WORDS.length)]
 console.log(rightGuessStr);
+
+
+function initBoard() {
+
+    let board = document.getElementById("game-board");
+
+    for (let i = 0; i < NUM_GUESSES; i++) {
+        let row = document.createElement("div");
+        row.className = "letter-row";
+
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div");
+            box.className = "letter-box";
+            row.appendChild(box);
+        }
+
+        board.appendChild(row);
+
+    }
+
+}
+
+initBoard();
