55fd8e049c5c8f3e41d0470595248cc59386b89b
m-dorsey <dorseymakenzie@gmail.com>
2022-03-05 13:17:04 -0500

    changed game board identifier from div.class to div.id

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ const NUM_GUESSES = 6;
 let guessesRemaining = NUM_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessStr = WORDS[ Math.floor( Math.random() * WORDS.length)]
-console.log(rightGuessStr);
+let rightGuessStr = WORDS[ Math.floor( Math.random() * WORDS.length )];
 
+console.log(rightGuessStr);
 
 function initBoard() {
 
     let board = document.getElementById("game-board");
 
     for (let i = 0; i < NUM_GUESSES; i++) {
+
         let row = document.createElement("div");
         row.className = "letter-row";
+
 
         for (let j = 0; j < 5; j++) {
             let box = document.createElement("div");
@@ -24,9 +26,7 @@ function initBoard() {
         }
 
         board.appendChild(row);
-
     }
-
 }
 
 initBoard();
